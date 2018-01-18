22869c9559630b72ebce88921907af4023e08f22
ABISWA10 <ABISWA10@KCHL840G43295NN.master.hmkad.hallmark.com>
2018-01-18 13:51:13 -0600

    Project Added

---
+++ crudapp/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+	  app.get('/hello', function(req, res) {
+	  res.send('Hello World')
+	})
+	  app.get('/', function(req, res) {
+	  res.sendFile(__dirname+'/index.html')
+	  
+	})
+})
