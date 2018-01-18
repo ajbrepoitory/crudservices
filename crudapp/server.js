c3ade713d6a27567ed13f3f8222047c1125c7a8e
ajbrepoitory <32651565+ajbrepoitory@users.noreply.github.com>
2018-01-18 14:18:28 -0600

    Update server.js

--- crudapp/server.js
+++ crudapp/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
+app.listen(8080, function() {
   console.log('listening on 3000')
 	  app.get('/hello', function(req, res) {
 	  res.send('Hello World')
