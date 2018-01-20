79053c4de4ba9648742835ceef4f90a793af47b8
ABISWA10 <ABISWA10@KCHL840G43295NN.master.hmkad.hallmark.com>
2018-01-19 20:05:45 -0600

    Updated

--- crudapp/server.js
+++ crudapp/server.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(8080, function() {
-  console.log('listening on 8080')
-	  app.get('/hello', function(req, res) {
-	  res.send('Hello World')
+MongoClient.connect(process.env.MONGOURL, (err, database) => {
+	// ... start the server
+	if (err) return console.log(err)
+	db = database.db('MONGODB')
+	app.listen(8080, function() {
+		console.log('listening on 8080')
+		app.get('/hello', function(req, res) {
+			res.send('Hello World')
+		})
+		app.get('/', function(req, res) {
+			res.sendFile(__dirname+'/index.html')
+
+		})
+		app.post('/quotes', (req, res) => {
+			db.collection('quotes').save(req.body, (err, result) => {
+				if (err) return console.log(err)
+
+				console.log('saved to database')
+				res.redirect('/')
+			})
+		})
+		app.get('/quotes', (req, res) => {
+			db.collection('quotes').find().toArray(function(err, results) {
+				console.log(results)
+				res.send(results)
+				//res.redirect('/')
+			})
+		})
+		
 	})
-	  app.get('/', function(req, res) {
-	  res.sendFile(__dirname+'/index.html')
-	  
-	})
+
 })
